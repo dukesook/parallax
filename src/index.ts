98f45085124c344e0a586b37b9f45a6046716407
dukesook <devonsookhoo14@gmailcom>
2025-05-31 21:32:07 -0600

    debug button

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { loadGoogleMapsScript } from './use_gmaps';
 import { runRdfExample } from './rdf_handler';
 
+const debugButton = document.getElementById('debug-button');
+
+debugButton?.addEventListener('click', () => {
+  console.log('Debug button clicked');
+})
+
 loadGoogleMapsScript();
 
 runRdfExample();

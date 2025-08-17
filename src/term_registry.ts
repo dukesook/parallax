9c9b9b75205510d373ed5e07db57c42ae16d32fc
dukesook <devonsookhoo14@gmailcom>
2025-08-17 12:37:24 -0600

    TermRegistry: getIRI() & getLabel()

--- src/term_registry.ts
+++ src/term_registry.ts
@@ -1,1 +1,28 @@
-import * as bimap from '@rimbu/bimap';
+import * as Bimap from '@rimbu/bimap';
+// Documentation: https://rimbu.org/docs/collections/bimap
+
+// prettier-ignore
+const g_bimap = Bimap.bimap<string, string>().of(
+//['key', 'value']
+  ['iri1', 'value1'],
+  ['iri2', 'value2'],
+  ['iri3', 'value3'],
+  ['iri4', 'value4'],
+  ['iri5', 'value5'],
+);
+
+export function getIRI(label: string): string {
+  const iri = g_bimap.keyKey(label);
+  if (!iri) {
+    throw new Error(`IRI not found for label: ${label}`);
+  }
+  return iri;
+}
+
+export function getLabel(iri: string): string {
+  const label = g_bimap.getValue(iri);
+  if (!label) {
+    throw new Error(`Label not found for IRI: ${iri}`);
+  }
+  return label;
+}

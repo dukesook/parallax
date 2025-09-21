5c2839895dc7b83d8272c0a82b3e67be050553ad
dukesook <devonsookhoo14@gmail.com>
2025-09-21 07:57:22 -0600

    New File: fabricator.ts

---
+++ src/fabricator.ts
@@ -0,0 +1,24 @@
+function generateData() {
+  generateShips();
+  generatePorts();
+  generateTrips();
+}
+
+function generateShips() {
+  console.log('generateShips()');
+}
+
+function generatePorts() {
+  console.log('generatePorts()');
+  const ports = [
+    { port_id: 'P001', name: 'Port of Los Angeles', country: 'USA', latitude: 33.7361, longitude: -118.2631 },
+    { port_id: 'P002', name: 'Port of Shanghai', country: 'China', latitude: 31.2304, longitude: 121.4737 },
+    { port_id: 'P003', name: 'Port of Rotterdam', country: 'Netherlands', latitude: 51.948, longitude: 4.1345 },
+    { port_id: 'P004', name: 'Port of Singapore', country: 'Singapore', latitude: 1.2644, longitude: 103.82 },
+    { port_id: 'P005', name: 'Port of Sydney', country: 'Australia', latitude: -33.8523, longitude: 151.2108 },
+  ];
+}
+
+function generateTrips() {
+  console.log('generateTrips()');
+}
